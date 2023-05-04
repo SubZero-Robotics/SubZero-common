78c83d17f00f4316d552908380de670d8aa89438
WowMuchDoge <97766806+WowMuchDoge@users.noreply.github.com>
2023-05-03 20:27:46 -0500

    Implement stop command

--- robot/src/main/cpp/MultiAxisSubsystem.cpp
+++ robot/src/main/cpp/MultiAxisSubsystem.cpp
@@ -24,3 +24,23 @@ frc2::CommandPtr MultiAxisSubsystem::SetPose(std::span<Pose> poses) {
   }
   return cmd;
 }
+
+frc2::CommandPtr MultiAxisSubsystem::Stop(AxisIndex index) {
+  return frc2::InstantCommand(
+    [this] {
+      _axes[index]->StopMovement();
+    },
+    {this, _axes}
+  ).ToPtr();
+}
+
+frc2::CommandPtr MultiAxisSubsystem::Stop() {
+  auto cmd = frc2::RunCommand([] {}, {}).ToPtr();
+  if (poses.empty()) {
+    return cmd;
+  }
+  for (int i = 0; i < size(_axes); i++) {
+        cmd = std::move(cmd).AlongWith(Stop(i));
+  };
+  return cmd;
+}
