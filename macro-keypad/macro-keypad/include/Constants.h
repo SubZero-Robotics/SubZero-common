18e46b57ffba1170bc25390b789708f3daadfd02
having11 <gatoninja236@gmail.com>
2024-02-04 18:15:41 -0600

    initial

---
+++ macro-keypad/macro-keypad/include/Constants.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <Arduino.h>
+
+constexpr uint8_t ColCount = 3;
+constexpr uint8_t RowCount = 5;
+
+namespace Pins {
+    constexpr uint8_t Led = 27;
+
+    static const uint8_t ColumnPins[ColCount] = { 0, 1, 2, };
+    static const uint8_t RowPins[RowCount] = { 18, 19, 20, 21, 22, };
+}
+
+namespace Keys {
+    constexpr uint16_t DebounceDelay = 50;
+
+    enum class GamepadButton {
+        None = -1,
+        ButtonA = 0,
+        ButtonB = 1,
+        ButtonX = 2,
+        ButtonY = 3,
+        ButtonBumperLeft = 4,
+        ButtonBumperRight = 5,
+        ButtonBack = 6,
+        ButtonStart = 7,
+        ButtonLeftStick = 8,
+        ButtonRightStick = 9,
+    };
+}
+
+typedef struct {
+    uint8_t currentState;
+    uint8_t lastSteadyState = 0;
+    uint8_t lastFlickerState = 0;
+    Keys::GamepadButton button = Keys::GamepadButton::None;
+    uint32_t timestamp = 0;
+} KeypadKey_t;
+
+static KeypadKey_t keys[RowCount][ColCount];
