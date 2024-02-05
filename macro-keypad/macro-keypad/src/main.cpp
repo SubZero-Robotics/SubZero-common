34cb9cf2ab3443bdf3ffe9a950d718e04719fc8a
Lint Action <lint-action@samuelmeuli.com>
2024-02-05 00:18:02 +0000

    Fix code style issues with clang_format

--- macro-keypad/macro-keypad/src/main.cpp
+++ macro-keypad/macro-keypad/src/main.cpp
@@ -10,13 +10,36 @@ using namespace Keys;
 
 // Row 4 column 1 is not connected to anything
 
-static GamepadButton keyMapping[RowCount][ColCount] =
-{
-    /* Row 0 */ { GamepadButton::None, GamepadButton::None, GamepadButton::None, },
-    /* Row 1 */ { GamepadButton::None, GamepadButton::None, GamepadButton::None, },
-    /* Row 2 */ { GamepadButton::None, GamepadButton::None, GamepadButton::None, },
-    /* Row 3 */ { GamepadButton::None, GamepadButton::None, GamepadButton::None, },
-    /* Row 4 */ { GamepadButton::None, GamepadButton::None, GamepadButton::None, },
+static GamepadButton keyMapping[RowCount][ColCount] = {
+    /* Row 0 */ {
+        GamepadButton::None,
+        GamepadButton::None,
+        GamepadButton::None,
+    },
+    /* Row 1 */
+    {
+        GamepadButton::None,
+        GamepadButton::None,
+        GamepadButton::None,
+    },
+    /* Row 2 */
+    {
+        GamepadButton::None,
+        GamepadButton::None,
+        GamepadButton::None,
+    },
+    /* Row 3 */
+    {
+        GamepadButton::None,
+        GamepadButton::None,
+        GamepadButton::None,
+    },
+    /* Row 4 */
+    {
+        GamepadButton::None,
+        GamepadButton::None,
+        GamepadButton::None,
+    },
 };
 
 static Joystick_ gamepad;
@@ -25,71 +48,68 @@ static void configureKeys(void);
 static void processKey(uint8_t, uint8_t);
 
 void setup() {
-    pinMode(Pins::Led, OUTPUT);
-    digitalWriteFast(Pins::Led, HIGH);
+  pinMode(Pins::Led, OUTPUT);
+  digitalWriteFast(Pins::Led, HIGH);
 
-    for (uint8_t i = 0; i < ColCount; i++) {
-        pinMode(Pins::ColumnPins[i], OUTPUT);
-        digitalWriteFast(Pins::ColumnPins[i], LOW);
-    }
+  for (uint8_t i = 0; i < ColCount; i++) {
+    pinMode(Pins::ColumnPins[i], OUTPUT);
+    digitalWriteFast(Pins::ColumnPins[i], LOW);
+  }
 
-    for (uint8_t i = 0; i < RowCount; i++) {
-        pinMode(Pins::RowPins[i], INPUT_PULLDOWN);
-    }
+  for (uint8_t i = 0; i < RowCount; i++) {
+    pinMode(Pins::RowPins[i], INPUT_PULLDOWN);
+  }
 
-    configureKeys();
+  configureKeys();
 
-    gamepad.begin();
+  gamepad.begin();
 }
 
 void loop() {
-    for (uint8_t col = 0; col < ColCount; col++) {
-        digitalWriteFast(Pins::ColumnPins[col], HIGH);
-        for (uint8_t row = 0; row < RowCount; row++) {
-            processKey(row, col);
-        }
-        digitalWriteFast(Pins::ColumnPins[col], LOW);
+  for (uint8_t col = 0; col < ColCount; col++) {
+    digitalWriteFast(Pins::ColumnPins[col], HIGH);
+    for (uint8_t row = 0; row < RowCount; row++) {
+      processKey(row, col);
     }
+    digitalWriteFast(Pins::ColumnPins[col], LOW);
+  }
 }
 
 void configureKeys() {
-    for (uint8_t row = 0; row < RowCount; row++) {
-        for (uint8_t col = 0; col < ColCount; col++) {
-            keys[row][col] = {
-                .currentState = 0,
-                .lastSteadyState = 0,
-                .lastFlickerState = 0,
-                .button = keyMapping[row][col],
-                .timestamp = 0
-            };
-        }
+  for (uint8_t row = 0; row < RowCount; row++) {
+    for (uint8_t col = 0; col < ColCount; col++) {
+      keys[row][col] = {.currentState = 0,
+                        .lastSteadyState = 0,
+                        .lastFlickerState = 0,
+                        .button = keyMapping[row][col],
+                        .timestamp = 0};
     }
+  }
 }
 
 void processKey(uint8_t row, uint8_t col) {
-    auto* key = &keys[row][col];
+  auto *key = &keys[row][col];
 
-    if (key->button == GamepadButton::None) {
-        return;
+  if (key->button == GamepadButton::None) {
+    return;
+  }
+
+  key->currentState = digitalRead(Pins::RowPins[row]);
+
+  if (key->currentState != key->lastFlickerState) {
+    key->timestamp = millis();
+    key->lastFlickerState = key->currentState;
+  }
+
+  if ((millis() - key->timestamp) > DebounceDelay) {
+    if (key->lastSteadyState && !key->currentState) {
+      // Pressed
+      gamepad.setButton(static_cast<uint8_t>(key->button), 1);
+    } else if (!key->lastSteadyState && key->currentState) {
+      // Released
+      gamepad.setButton(static_cast<uint8_t>(key->button), 0);
     }
 
-    key->currentState = digitalRead(Pins::RowPins[row]);
-
-    if (key->currentState !=  key->lastFlickerState) {
-        key->timestamp = millis();
-        key->lastFlickerState = key->currentState;
-    }
-
-    if ((millis() - key->timestamp) > DebounceDelay) {
-        if (key->lastSteadyState && !key->currentState) {
-            // Pressed
-            gamepad.setButton(static_cast<uint8_t>(key->button), 1);
-        }
-        else if (!key->lastSteadyState && key->currentState) {
-            // Released
-            gamepad.setButton(static_cast<uint8_t>(key->button), 0);
-        }
-
-        key->lastSteadyState = key->currentState;
-    }
+    key->lastSteadyState = key->currentState;
+  }
 }
