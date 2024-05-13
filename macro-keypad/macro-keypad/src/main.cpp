9b5dd761eaa5d1843cc35e732e342bb7ac3ca155
5690 <5690Programmers@gmail.com>
2024-05-13 17:54:04 -0500

    Cleanup and begin bringing over common utilities from 2024 robot

---
+++ macro-keypad/macro-keypad/src/main.cpp
@@ -0,0 +1,115 @@
+#include <Arduino.h>
+#include <Joystick.h>
+
+#include "Constants.h"
+
+using namespace Keys;
+
+// Col 0 starts on the left
+// Row 0 starts at the top
+
+// Row 4 column 1 is not connected to anything
+
+static GamepadButton keyMapping[RowCount][ColCount] = {
+    /* Row 0 */ {
+        GamepadButton::ButtonA,
+        GamepadButton::ButtonVirtual4,
+        GamepadButton::ButtonX,
+    },
+    /* Row 1 */
+    {
+        GamepadButton::ButtonY,
+        GamepadButton::ButtonBumperLeft,
+        GamepadButton::ButtonBumperRight,
+    },
+    /* Row 2 */
+    {
+        GamepadButton::ButtonBack,
+        GamepadButton::ButtonStart,
+        GamepadButton::ButtonLeftStick,
+    },
+    /* Row 3 */
+    {
+        GamepadButton::ButtonRightStick,
+        GamepadButton::ButtonVirtual0,
+        GamepadButton::ButtonVirtual1,
+    },
+    /* Row 4 */
+    {
+        GamepadButton::ButtonVirtual2,
+        GamepadButton::None,
+        GamepadButton::ButtonVirtual3,
+    },
+};
+
+static Joystick_ gamepad;
+
+static void configureKeys(void);
+static void processKey(uint8_t, uint8_t);
+
+void setup() {
+  pinMode(Pins::Led, OUTPUT);
+  digitalWriteFast(Pins::Led, HIGH);
+
+  for (uint8_t i = 0; i < ColCount; i++) {
+    pinMode(Pins::ColumnPins[i], OUTPUT);
+    digitalWriteFast(Pins::ColumnPins[i], LOW);
+  }
+
+  for (uint8_t i = 0; i < RowCount; i++) {
+    pinMode(Pins::RowPins[i], INPUT_PULLDOWN);
+  }
+
+  configureKeys();
+
+  gamepad.begin();
+}
+
+void loop() {
+  for (uint8_t col = 0; col < ColCount; col++) {
+    digitalWriteFast(Pins::ColumnPins[col], HIGH);
+    for (uint8_t row = 0; row < RowCount; row++) {
+      processKey(row, col);
+    }
+    digitalWriteFast(Pins::ColumnPins[col], LOW);
+  }
+}
+
+void configureKeys() {
+  for (uint8_t row = 0; row < RowCount; row++) {
+    for (uint8_t col = 0; col < ColCount; col++) {
+      keys[row][col] = {.currentState = 0,
+                        .lastSteadyState = 0,
+                        .lastFlickerState = 0,
+                        .button = keyMapping[row][col],
+                        .timestamp = 0};
+    }
+  }
+}
+
+void processKey(uint8_t row, uint8_t col) {
+  auto *key = &keys[row][col];
+
+  if (key->button == GamepadButton::None) {
+    return;
+  }
+
+  key->currentState = digitalRead(Pins::RowPins[row]);
+
+  if (key->currentState != key->lastFlickerState) {
+    key->timestamp = millis();
+    key->lastFlickerState = key->currentState;
+  }
+
+  if ((millis() - key->timestamp) > DebounceDelay) {
+    if (key->lastSteadyState && !key->currentState) {
+      // Released
+      gamepad.setButton(static_cast<uint8_t>(key->button), 0);
+    } else if (!key->lastSteadyState && key->currentState) {
+      // Pressed
+      gamepad.setButton(static_cast<uint8_t>(key->button), 1);
+    }
+
+    key->lastSteadyState = key->currentState;
+  }
+}
---
+++ subzero/include/constants/ColorConstants.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <frc/util/Color.h>
+
+namespace ColorConstants {
+static const frc::Color8Bit kYellow{0xE5, 0xD5, 0x24};
+static const frc::Color8Bit kPurple{0x57, 0x14, 0xE8};
+static const frc::Color8Bit kRed{0xFC, 0x19, 0x19};
+static const frc::Color8Bit kGreen{0x28, 0xCC, 0x2E};
+static const frc::Color8Bit kBlue{0x08, 0x3B, 0xD3};
+static const frc::Color8Bit kTeal{0x18, 0x9B, 0xCE};
+static const frc::Color8Bit kOrange{0xE8, 0x61, 0x19};
+static const frc::Color8Bit kAcidGreen{0xB0, 0xBF, 0x1A};
+static const frc::Color8Bit kBlack{0x00, 0x00, 0x00};
+}  // namespace ColorConstants
