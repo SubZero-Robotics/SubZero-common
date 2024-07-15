22d7603b086c733277e2cd4d84dc2d7a18408b24
having11 <gatoninja236@gmail.com>
2024-07-15 17:04:25 -0500

    add 3d positioning

--- lib/cpp/subzero/singleaxis/LinearSingleAxisSubsystem.cpp
+++ lib/cpp/subzero/singleaxis/LinearSingleAxisSubsystem.cpp
@@ -2,6 +2,9 @@
 
 #include "subzero/singleaxis/LinearSingleAxisSubsystem.h"
 
+#include <frc/geometry/Pose3d.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+
 #include "subzero/singleaxis/BaseSingleAxisSubsystem.cpp"
 
 using namespace subzero;
@@ -12,20 +15,33 @@ void LinearSingleAxisSubsystem<TController>::Periodic() {
 
   LinearBase::Periodic();
 
+  auto length =
+      ((LinearBase::m_config.reversed ? -(LinearBase::GetCurrentPosition())
+                                      : LinearBase::GetCurrentPosition()) +
+       LinearBase::m_config.mechanismConfig.minimumLength);
+
   if (LinearBase::m_ligament2d) {
-    LinearBase::m_ligament2d->SetLength(
-        ((LinearBase::m_config.reversed ? -(LinearBase::GetCurrentPosition())
-                                        : LinearBase::GetCurrentPosition()) +
-         LinearBase::m_config.mechanismConfig.minimumLength)
-            .value());
+    LinearBase::m_ligament2d->SetLength(length.value());
   }
+
+  frc::Rotation3d rot{0_rad, 0_rad, 0_rad};
+  frc::Pose3d pose{
+      0_m, 0_m, (length - LinearBase::m_config.mechanismConfig.minimumLength),
+      rot};
+  std::vector<double> data{pose.X().value(),        pose.Y().value(),
+                           pose.Z().value(),        rot.GetQuaternion().W(),
+                           rot.GetQuaternion().X(), rot.GetQuaternion().Y(),
+                           rot.GetQuaternion().Z()};
+  frc::SmartDashboard::PutNumberArray(LinearBase::m_name + "_position", data);
 }
 
 template <typename TController>
 void LinearSingleAxisSubsystem<TController>::RunMotorVelocity(
     units::meters_per_second_t speed, bool ignoreEncoder) {
-  BaseSingleAxisSubsystem<TController, units::meter>::DisablePid();
+  using LinearBase = BaseSingleAxisSubsystem<TController, units::meter>;
+
+  LinearBase::DisablePid();
   ConsoleWriter.logWarning(
-      BaseSingleAxisSubsystem<TController, units::meter>::m_name,
+      LinearBase::m_name,
       "Running with a velocity is not supported for linear subsystems!%s", "");
 }
--- lib/cpp/subzero/singleaxis/RotationalSingleAxisSubsystem.cpp
+++ lib/cpp/subzero/singleaxis/RotationalSingleAxisSubsystem.cpp
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "subzero/singleaxis/RotationalSingleAxisSubsystem.h"
+
+#include <frc/geometry/Pose3d.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+
 #include "subzero/singleaxis/BaseSingleAxisSubsystem.cpp"
 
 using namespace subzero;
@@ -11,13 +15,22 @@ void RotationalSingleAxisSubsystem<TController>::Periodic() {
 
   RotationalBase::Periodic();
 
+  auto angle = (RotationalBase::m_config.reversed
+                    ? -RotationalBase::GetCurrentPosition()
+                    : RotationalBase::GetCurrentPosition()) +
+               RotationalBase::m_config.mechanismConfig.minimumAngle;
   if (RotationalBase::m_ligament2d) {
-    RotationalBase::m_ligament2d->SetAngle(
-        (RotationalBase::m_config.reversed
-             ? -RotationalBase::GetCurrentPosition()
-             : RotationalBase::GetCurrentPosition()) +
-        RotationalBase::m_config.mechanismConfig.minimumAngle);
+    RotationalBase::m_ligament2d->SetAngle(angle);
   }
+
+  frc::Rotation3d rot{0_rad, -angle - 30_deg, 0_rad};
+  frc::Pose3d pose{0_m, 0_m, 0_m, rot};
+  std::vector<double> data{pose.X().value(),        pose.Y().value(),
+                           pose.Z().value(),        rot.GetQuaternion().W(),
+                           rot.GetQuaternion().X(), rot.GetQuaternion().Y(),
+                           rot.GetQuaternion().Z()};
+  frc::SmartDashboard::PutNumberArray(RotationalBase::m_name + "_position",
+                                      data);
 }
 
 template <typename TController>
